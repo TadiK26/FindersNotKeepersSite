efac3745679b9bc66596903043338f7dd088f56d
Tadiwanashe Kabaira <u22490125@tuks.co.za>
2025-10-25 14:34:21 +0200

    Notification Interstage

--- Backend core/Notification.py
+++ Backend core/Notification.py
@@ -8,6 +8,18 @@ from dotenv import load_dotenv
 class Notification:
     """Handles sending notifications to users."""
     
+    @staticmethod
+    def get_connection():
+        load_dotenv()
+
+        return mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database="findersnotkeepers"
+        )
+    
+
     def SendClaimNoti(self):
         """Notify the owner of a listing when someone claims their item."""
         try:
@@ -176,3 +188,28 @@ class Notification:
             conn.close()
         except Exception as e:
             print(f"Error in sendToAdmin: {e}")
+
+    def send_email(self, recipient_email, subject, body):
+        load_dotenv()
+
+        SMTP_SERVER = "smtp.gmail.com"
+        SMTP_PORT = 587
+        EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+        EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+        try:
+            msg = MIMEMultipart()
+            msg['From'] = EMAIL_ADDRESS
+            msg['To'] = recipient_email
+            msg['Subject'] = subject
+            
+            msg.attach(MIMEText(body, 'plain'))
+
+            with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+                server.starttls()
+                server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                server.sendmail(EMAIL_ADDRESS, recipient_email, msg.as_string())
+
+            print("Email send successfully")
+        except Exception as e:
+            print(f"Error sending email: {e}")
